1a0b56e20e7d6fb71118c67e5141aa9e52a54594
Рыжанина Саша <ryganinaan@gmail.com>
2022-11-09 20:51:22 +0300

    actions

--- src/components/todo-list-sidebar.js
+++ src/components/todo-list-sidebar.js
@@ -5,7 +5,7 @@ export default function TodoListSidebar() {
     const name = ['ToDo List', 'ToDo List Projects'];
 
     return (
-        <div>
+        <div className="sidebar">
             {name.map((item, index)=> (
                 <SidebarItem name={item} key={index}></SidebarItem>
                 ))} 
